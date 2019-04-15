bd41c1be42b2a0cbfbe470bc2912be5bd6d10885
tnoyh <atnoyh@icloud.com>
2019-04-15 14:27:53 +0800

    alias 别名；单例 多例；ID name；scope；autowire

--- spring20190415hs/src/test/java/com/neuedu/entity/UserTest.java
+++ spring20190415hs/src/test/java/com/neuedu/entity/UserTest.java
@@ -44,4 +44,44 @@ public class UserTest {
         System.out.println(user.getUid()+"  "+user.getUname()+ "   "+user.getUpwd());;
     }
 
+    @Test
+    public  void  usertestbean(){
+        /*  1:创建一个ApplicationContext对象*/
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring-config.xml");
+        /*2：User对象的创建:     context.getBean中的值为xml中bean标签的id值
+         */
+        /*User user=(User)context.getBean("vipuser");*/  //不同的人调用不同的名字 结果还是一样的
+        User user=(User)context.getBean("user2 user3 user4"); //以多个名字命名
+        System.out.println(user.getUid()+"  "+user.getUname()+ "   "+user.getUpwd());;
+    }
+
+
+    //测试多例模式还是单例模式 输出看他们的地址是否一样
+    @Test
+    public  void  usertestscope(){
+        /*  1:创建一个ApplicationContext对象*/
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring-config.xml");
+        /*2：User对象的创建:     context.getBean中的值为xml中bean标签的id值
+         */
+        /*User user=(User)context.getBean("vipuser");*/
+        User user=(User)context.getBean("user");
+        User user2=(User)context.getBean("user");
+        System.out.println(user);
+        System.out.println(user2);
+    }
+
+
+    @Test
+    public  void  usertestautowire(){
+        /*  1:创建一个ApplicationContext对象*/
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring-config.xml");
+        /*2：User对象的创建:     context.getBean中的值为xml中bean标签的id值
+         */
+
+        User user=(User)context.getBean("user");
+
+        System.out.println(user.getUname());
+
+    }
+
 }
